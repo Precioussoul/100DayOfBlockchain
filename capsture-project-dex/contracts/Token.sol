e02c538f5fb90e09f10038d3f44ff4efe6cb8b79
Precioussoul <habsoff@gmail.com>
2024-01-24 22:39:27 +0100

    feat: test writing for smartcontract

--- capsture-project-dex/contracts/Token.sol
+++ capsture-project-dex/contracts/Token.sol
@@ -6,12 +6,15 @@ import "hardhat/console.sol";
 contract Token {
     string public name;
     string public symbol;
-    uint256 public decimal; 
+    uint256 public decimal = 18; 
     uint256 public totalSupply; 
+
+    mapping (address => uint256) public balanceOf;
   
-    constructor(string memory _name, string memory _symbol, uint256 memory _totalSupply) {
-        name = _name
-        symbol = _symbol
-        totalSupply = _totalSupply * (10**decimal)
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10**decimal);
+        balanceOf[msg.sender] = totalSupply;
     }
 }
--- capsture-project-dex/test/sample-test.js
+++ capsture-project-dex/test/sample-test.js
@@ -1,19 +1,35 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const {expect} = require("chai")
+const {ethers} = require("hardhat")
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether")
+}
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+describe("Token", function () {
+  const name = "Ethereum"
+  const symbol = "ETH"
+  const decimal = "18"
+  const totalSupply = tokens("100000")
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  let token, accounts, deployer
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+  beforeEach(async () => {
+    const Token = await ethers.getContractFactory("Token")
+    token = await Token.deploy("Ethereum", "ETH", 100000)
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
+  })
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+  it("has correct name", async function () {
+    expect(await token.name()).to.equal(name)
+  })
+  it("has correct symbol", async function () {
+    expect(await token.symbol()).to.equal(symbol)
+  })
+  it("has correct totalSuply", async function () {
+    expect(await token.totalSupply()).to.equal(totalSupply)
+  })
+  it("has correct token balance", async function () {
+    expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+  })
+})
